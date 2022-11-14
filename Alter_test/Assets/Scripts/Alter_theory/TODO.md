42672b4747e035f62677fcd779b783a81c4bd7cc
thygolem <thygolem@gmail.com>
2022-11-14 12:18:47 +0100

    corrutina prefabs TrainDixa

---
+++ Alter_test/Assets/Scripts/Alter_theory/TODO.md
@@ -0,0 +1,16 @@
+## ALTER THEORY
+- (2D) => Drag n drop cubes w/ a color in side Left canvas (Base).
+- screenCapture->toPNG must be donde when player hits "Done!"  
+- [screenCapture->toPNG](https://docs.unity3d.com/ScriptReference/ScreenCapture.CaptureScreenshot.html)
+
+### Each color means something - Use the color as main scale purpose.
+
+- Ionian        -> Orange 
+- Dorian        -> Yellow 
+- Phrygian      -> Brown
+- Lydian        -> Shiny Blue
+- Mixolydian    -> Deep Blue & Orange/Yellow  
+- Aeolian       -> Red 
+- Locrian       -> Awful Brown (like all painting colors mixed)
+
+
--- Alter_test/Assets/Scripts/TrainDixa/WorldManager.cs
+++ Alter_test/Assets/Scripts/TrainDixa/WorldManager.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// Elimina la ambugüedad de random
+using Random = UnityEngine.Random;
 
 
 namespace Scripts.TrainDixa
@@ -9,14 +11,44 @@ namespace Scripts.TrainDixa
 
     public class WorldManager : MonoBehaviour
     {
-        [SerializeField] BallController _ballController;
+        [SerializeField] GameObject _ballPrefab;
+        [SerializeField] Transform _ballPosition;
+        [SerializeField] int _MaxNumberOfBalls = 5;
+
+        int ballsCounter = 0;
 
         private void Awake()
         {
-            // Me suscribo al evento Action OnBallDestroyed. Haciendo _ballController.OnBallDestroyed
-                // El evento y el manejador suelen tener el mismo nombre
-            _ballController.OnBallDestroyedEvent += OnBallDestroyedHandler;
+
+            if (_MaxNumberOfBalls <= 0) throw new Exception("ERROR, número no válido");
+            StartCoroutine(CreateBall());
+
         }
+
+        IEnumerator CreateBall()
+        {
+            yield return new WaitForSeconds(1f);
+            ballsCounter++;
+            // Me suscribo con "+=" al evento Action OnBallDestroyed. Haciendo _ballController.OnBallDestroyed
+            // El evento y el manejador(handler) suelen tener el mismo nombre
+            // _ballPrefab.OnBallDestroyedEvent += OnBallDestroyedHandler;
+            ////////////////////////////////////////////////////////////////
+            float HorizontalVariation = Camera.main.orthographicSize * Camera.main.aspect * 0.8f;
+            // var sirve para crear variables locales, que solo van a poder ser usadas dentro del método donde se crean.
+            var xPosition = _ballPosition.position.x + Random.Range(-HorizontalVariation, HorizontalVariation);
+            var ballGO = Instantiate(_ballPrefab);
+            ballGO.transform.position = new Vector2(xPosition, _ballPosition.position.y);
+            ballGO.GetComponent<BallController>().OnBallDestroyedEvent += OnBallDestroyedHandler;
+            // Recursividad
+            if (ballsCounter < _MaxNumberOfBalls)
+            {
+                StartCoroutine(CreateBall());
+
+            }
+            
+
+        }
+
 
         private void OnBallDestroyedHandler() // estamos desacoplando objetos
         {
